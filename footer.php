333073641ab6e0f6ea501b90a9a69d4ffa3e98f2
rizkyyz <rizkyya509@gmail.com>
2024-01-27 23:30:36 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #6495ED;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #6495ED"><b>MADOS-CAKE FOOD</b></h3>
+					<p>Jl. Cempaka putih blok A1</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6287804616097</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> cakefood@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #6495ED; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Ba'do Febriansyah</span>
+		</div>
+	</footer>
+
+</body>
+</html>
